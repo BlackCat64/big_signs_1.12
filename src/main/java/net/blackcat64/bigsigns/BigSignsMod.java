614c54164984957fb2e8d1eebd7d6c2fc6ffdce8
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-04-15 18:58:34 +0100

    Changed mod ID and stuff

--- src/main/java/net/blackcat64/bigsigns/BigSignsMod.java
+++ src/main/java/net/blackcat64/bigsigns/BigSignsMod.java
@@ -1,19 +1,17 @@
-package net.darkhax.examplemod;
+package net.blackcat64.bigsigns;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ModContainer;
 
-@Mod(modid = ExampleMod.MOD_ID)
-public class ExampleMod {
+@Mod(modid = BigSignsMod.MOD_ID)
+public class BigSignsMod {
     
-    public static final String MOD_ID = "examplemod";
-    public static final Logger LOGGER = LogManager.getLogger("Example Mod");
+    public static final String MOD_ID = "big_signs";
+    public static final Logger LOGGER = LogManager.getLogger("Big Signs");
     
-    public ExampleMod() {
+    public BigSignsMod() {
         
 //    	final ModContainer container = Loader.instance().getIndexedModList().get(MOD_ID);
 //    	LOGGER.info("Loaded {} v{}", container.getName(), container.getDisplayVersion());
