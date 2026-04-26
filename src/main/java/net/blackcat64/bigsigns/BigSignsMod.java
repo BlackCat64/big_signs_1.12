d97d8e9e1545a9c4c3cbca97c00e449e1f974f2c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2026-04-26 14:10:30 +0100

    Initial commit

---
+++ src/main/java/net/blackcat64/bigsigns/BigSignsMod.java
@@ -0,0 +1,31 @@
+package net.blackcat64.bigsigns;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BigSignsMod.MODID, name = BigSignsMod.NAME, version = BigSignsMod.VERSION)
+public class BigSignsMod
+{
+    public static final String MODID = "big_signs";
+    public static final String NAME = "Big Signs (1.12 Port)";
+    public static final String VERSION = "1.2";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
